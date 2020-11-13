6e3f67b78ae32b033e1c854a4cf7e1f01457461f
DragosBadarau <74381554+DragosBadarau@users.noreply.github.com>
2020-11-13 22:18:02 +0200

    slider

--- slider.js
+++ slider.js
@@ -1,9 +1,9 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
- function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+    }
 
 function currentSlide(n) {
   showSlides(slideIndex = n);
